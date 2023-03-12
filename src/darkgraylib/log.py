59e59b702d1c39573c17d7aedd0525bd3051c5d2
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2023-03-12 17:13:51 +0200

    Implement logging support
    Called from `darker.__main__` and `graylint.__main__`

---
+++ src/darkgraylib/log.py
@@ -0,0 +1,8 @@
+import logging
+
+
+def setup_logging(log_level: int) -> None:
+    logging.basicConfig(level=log_level)
+    if log_level == logging.INFO:
+        formatter = logging.Formatter("%(levelname)s: %(message)s")
+        logging.getLogger().handlers[0].setFormatter(formatter)
