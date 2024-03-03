4f51c3f55014da2e10cfe63529690b07c3c6d7d6
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2024-03-03 19:42:04 +0200

    Move `raises_or_matches()` test helper from Darker

--- src/darkgraylib/testtools/helpers.py
+++ src/darkgraylib/testtools/helpers.py
@@ -1,11 +1,34 @@
 """Helper functions for unit tests"""
 
 import re
-from contextlib import nullcontext
-from typing import Any, ContextManager, Dict, Union
+from contextlib import contextmanager, nullcontext
+from typing import Any, ContextManager, Dict, List, Union
 
 import pytest
 from _pytest.python_api import RaisesContext
+
+
+def matching_attrs(obj: BaseException, attrs: List[str]) -> Dict[str, int]:
+    """Return object attributes whose name matches one in the given list"""
+    return {attname: getattr(obj, attname) for attname in dir(obj) if attname in attrs}
+
+
+@contextmanager
+def raises_or_matches(expect, match_exc_attrs):
+    """Helper for matching an expected value or an expected raised exception"""
+    if isinstance(expect, BaseException):
+        with pytest.raises(type(expect)) as exc_info:
+            # The lambda callback should never get called
+            yield lambda result: False
+        exception_attributes = matching_attrs(exc_info.value, match_exc_attrs)
+        expected_attributes = matching_attrs(expect, match_exc_attrs)
+        assert exception_attributes == expected_attributes
+    else:
+
+        def check(result):
+            assert result == expect
+
+        yield check
 
 
 def filter_dict(dct: Dict[str, Any], filter_key: str) -> Dict[str, Any]:
