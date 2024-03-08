d4020e803138028bee81ba52a0ae02b9296f5115
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2024-03-08 16:41:34 +0200

    Turn `TestCase` to a Pytest style function

--- src/darkgraylib/tests/test_files.py
+++ src/darkgraylib/tests/test_files.py
@@ -5,7 +5,6 @@ from contextlib import contextmanager
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from typing import Iterator
-from unittest import TestCase
 
 from darkgraylib import files
 
@@ -21,46 +20,33 @@ def change_directory(path: Path) -> Iterator[None]:
         os.chdir(previous_dir)
 
 
-class BlackTestCase(TestCase):
-    def test_find_project_root(self) -> None:
-        with TemporaryDirectory() as workspace:
-            root = Path(workspace)
-            test_dir = root / "test"
-            test_dir.mkdir()
+def test_find_project_root() -> None:
+    with TemporaryDirectory() as workspace:
+        root = Path(workspace)
+        test_dir = root / "test"
+        test_dir.mkdir()
 
-            src_dir = root / "src"
-            src_dir.mkdir()
+        src_dir = root / "src"
+        src_dir.mkdir()
 
-            root_pyproject = root / "pyproject.toml"
-            root_pyproject.write_text("[tool.black]", encoding="utf-8")
-            src_pyproject = src_dir / "pyproject.toml"
-            src_pyproject.write_text("[tool.black]", encoding="utf-8")
-            src_python = src_dir / "foo.py"
-            src_python.touch()
+        root_pyproject = root / "pyproject.toml"
+        root_pyproject.write_text("[tool.black]", encoding="utf-8")
+        src_pyproject = src_dir / "pyproject.toml"
+        src_pyproject.write_text("[tool.black]", encoding="utf-8")
+        src_python = src_dir / "foo.py"
+        src_python.touch()
 
-            self.assertEqual(
-                files.find_project_root((src_dir, test_dir)),
-                root.resolve(),
-            )
-            self.assertEqual(
-                files.find_project_root((src_dir,)),
-                src_dir.resolve(),
-            )
-            self.assertEqual(
-                files.find_project_root((src_python,)),
-                src_dir.resolve(),
-            )
+        assert files.find_project_root((src_dir, test_dir)) == root.resolve()
+        assert files.find_project_root((src_dir,)) == src_dir.resolve()
+        assert files.find_project_root((src_python,)) == src_dir.resolve()
 
-            src_sub = src_dir / "sub"
-            src_sub.mkdir()
+        src_sub = src_dir / "sub"
+        src_sub.mkdir()
 
-            # src_sub_pyproject = src_sub / "pyproject.toml"
-            # src_sub_pyproject.touch()  # empty
+        # src_sub_pyproject = src_sub / "pyproject.toml"
+        # src_sub_pyproject.touch()  # empty
 
-            src_sub_python = src_sub / "bar.py"
+        src_sub_python = src_sub / "bar.py"
 
-            # we skip src_sub_pyproject since it is missing the [tool.black] section
-            self.assertEqual(
-                files.find_project_root((src_sub_python,)),
-                src_dir.resolve(),
-            )
+        # we skip src_sub_pyproject since it is missing the [tool.black] section
+        assert files.find_project_root((src_sub_python,)) == src_dir.resolve()
