f59b07eb6c0f35bdee096bf992f07193ee122a68
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2024-04-15 20:23:57 +0300

    Improve docstring of `resolve_paths()`

--- src/darkgraylib/main.py
+++ src/darkgraylib/main.py
@@ -9,12 +9,13 @@ from darkgraylib.utils import get_common_root
 def resolve_paths(
     stdin_filename: Optional[str], src: List[str]
 ) -> Tuple[Set[Path], Path]:
-    """Resolve paths from the ``src`` and ``--stdin-filename`` command line arguments
+    """Return paths + common root for ``--stdin-filename`` or ``src`` cmdline arguments
 
     :param stdin_filename: The value of the ``--stdin-filename`` command line argument,
                            or ``None`` if the option was not given
     :param src: The value of the ``src`` command line argument
-    :return: A tuple of the resolved paths and the common root directory of the paths
+    :return: A tuple of: 1) original paths from either command line arguments, and
+             2) the absolute path to the deepest common root directory of the paths.
 
     """
     if stdin_filename is not None:
